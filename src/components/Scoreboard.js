22540592bc90fe28aaeed1f095d54d9abb26b19c
Lindsay Lopez <lopez95@berkeley.edu>
2021-11-11 12:44:41 -0800

    styled scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../styles/Scoreboard.css';
 
 function Scoreboard() {
   return (
     <div className="Scoreboard">
       <p>Current Score: 0</p>
+      <i className="fas fa-bolt"></i>
       <p>Best Score: 0</p>
+      <i className="fas fa-bolt"></i>
       <p>Level 1 Score: 0 / 4</p>
     </div>
   );
