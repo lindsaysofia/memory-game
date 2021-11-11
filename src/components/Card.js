d67e368187e642eacdd4585546a565c32c706f7c
Lindsay Lopez <lopez95@berkeley.edu>
2021-11-11 13:04:08 -0800

    styled Gameboard

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../styles/Card.css';
 
 function Card() {
   return (
     <div className="Card">
-      <img alt=""/>
+      <img 
+        alt=""
+        src="https://storage.googleapis.com/ygoprodeck.com/pics_small/34541863.jpg"
+      />
       <p>Description</p>
     </div>
   );
--- src/components/Gamboard.js
+++ src/components/Gamboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import '../styles/Gameboard.css'
 
 function Gameboard() {
   return (
